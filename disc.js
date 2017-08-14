4eb681f549423e11c254f86cb5d39105a69465a1
Herbert Spencer <hspencer@ead.cl>
2017-08-14 19:37:12 -0300

    multicolor

--- disc.js
+++ disc.js
@@ -5,7 +5,7 @@
 */
 
 
-var canvas, disc, t, inc, weight;
+var canvas, disc, t, inc, weight, sc;
 var x1, y1, x2, y2, xa, ya, xb, yb;
 
 
@@ -15,7 +15,6 @@ function setup() {
   t = 0;
   disc = createGraphics(500, 500);
   discReset();
-  background(0);
   noFill();
 }
 
@@ -30,6 +29,10 @@ function draw() {
   weight = parseFloat(document.getElementById("weight").value);
   disc.strokeWeight(weight);
 
+  sc = document.getElementById("strokeColor").value;
+  disc.stroke(sc); 
+  print(sc);
+
   x1 = mouseX - width/2;
   y1 = mouseY - height/2;
   x2 = pmouseX - width/2;
@@ -40,14 +43,13 @@ function draw() {
   xb = width/2 + x2*cos(t) - y2*sin(t);
   yb = height/2 + x2*sin(t) + y2*cos(t);
 
+  if (mouseIsPressed && dist(mouseX,mouseY,width/2,height/2)<width/2) {
+	  disc.line(xa, ya, xb, yb);
+	}
+
   push();
   {
     translate(width/2, height/2);
-
-    if (mouseIsPressed && dist(mouseX,mouseY,width/2,height/2)<width/2) {
-      disc.stroke(0, 200);
-      disc.line(xa, ya, xb, yb);
-    }
     rotate(-t);
     image(disc, -width/2, -height/2);
   }
@@ -55,17 +57,17 @@ function draw() {
 }
 
 function touchMoved(){
-  disc.stroke(0, 200);
   disc.line(xa, ya, xb, yb);
 }
 
 function discReset() {
+  background(0);
+  disc.clear();
   push();
   translate(width/2, height/2);
   disc.stroke(0, 50);
   disc.strokeWeight(1);
   disc.noFill();
-  disc.background(0);
   disc.fill(255);
   disc.ellipse(width/2, height/2, disc.width, disc.height);
   disc.line(0, height/2, width, height/2);
@@ -78,6 +80,6 @@ function clearDisc() {
 }
 
 function saveDisc(){
-  var name = "disc-"+year()+"-"+month()+"-"+day()+"-"+hour()+"-"+minute()+"-"+second()+".png"
-  save(name);
+  var filename = "disc-"+year()+"-"+month()+"-"+day()+"-"+hour()+"-"+minute()+"-"+second()+".png"
+  save(disc, filename);
 }
